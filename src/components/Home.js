0e807b476d5ff2871c53122a204984974d8f5d33
Simamkele44 <simamkele44@gmail.com>
2021-07-19 07:04:04 +0200

    first

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            <Section
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panels in SA"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Accesories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Order Now"
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height:100vh;
+
+`
